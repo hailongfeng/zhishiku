ad77bdeedf0e56a5c597347e0870cd295772bf96
hailongfeng <no_1hailong@yeah.net>
2018-10-30 08:58:40 +0800

    Android 自带底部导航View的使用+viewPager

---
+++ Android/插件研究/BottomNavigationDemo/app/src/main/java/com/afanbaby/bottomnavigationdemo/BottomNavigationViewHelper.java
@@ -0,0 +1,40 @@
+package com.afanbaby.bottomnavigationdemo;
+
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by afanbaby on 2018/2/2.
+ * 使用的时候 item 数大于 3 个时会有位移，使用下面的类通过反射来修改
+ */
+
+public class BottomNavigationViewHelper {
+
+    public static void disableShiftMode(BottomNavigationView view) {
+
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVHelper", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
+        }
+    }
+}
---
+++ Android/插件研究/BottomNavigationDemo/app/src/main/java/com/afanbaby/bottomnavigationdemo/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.afanbaby.bottomnavigationdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by afanbaby on 2018/2/2.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> list;
+
+    public void setList(List<Fragment> list) {
+        this.list = list;
+        notifyDataSetChanged();
+    }
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return list != null ? list.size() : 0;
+    }
+}
