f1eea254025dd4ffbc18c0de2048d443d59c0b1c
hailongfeng <no_1hailong@yeah.net>
2018-10-31 19:04:11 +0800

    消息通知高版本兼容性问题。

---
+++ Android/插件研究/NotificationTest/src/main/java/com/fhl/notificationtest/MainActivity.java
@@ -0,0 +1,87 @@
+package com.fhl.notificationtest;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "chat";
+            String channelName = "聊天消息";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            createNotificationChannel(channelId, channelName, importance);
+
+            channelId = "subscribe";
+            channelName = "订阅消息";
+            importance = NotificationManager.IMPORTANCE_DEFAULT;
+            createNotificationChannel(channelId, channelName, importance);
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel(String channelId, String channelName, int importance) {
+        NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(
+                NOTIFICATION_SERVICE);
+        channel.setShowBadge(true);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    public void sendChatMsg(View view) {
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = manager.getNotificationChannel("chat");
+            if (channel.getImportance() == NotificationManager.IMPORTANCE_NONE) {
+                Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+                intent.putExtra(Settings.EXTRA_CHANNEL_ID, channel.getId());
+                startActivity(intent);
+                Toast.makeText(this, "请手动将通知打开", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+        Notification notification = new NotificationCompat.Builder(this, "chat")
+                .setContentTitle("收到一条聊天消息")
+                .setContentText("今天中午吃什么？")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.shoucang)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.shoucang))
+                .setAutoCancel(true)
+                .build();
+        manager.notify(1, notification);
+    }
+
+    public void sendSubscribeMsg(View view) {
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this, "subscribe")
+                .setContentTitle("收到一条订阅消息")
+                .setContentText("地铁沿线30万商铺抢购中！")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.shoucang)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.shoucang))
+                .setAutoCancel(true)
+                .setNumber(2)
+                .build();
+        manager.notify(2, notification);
+    }
+
+    public void sendToastMsg(View view) {
+        Toast.makeText(this, "toast弹出消息", Toast.LENGTH_SHORT).show();
+    }
+}
